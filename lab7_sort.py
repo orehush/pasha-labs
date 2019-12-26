faec69a1071689be0d51e840700d00d579cdc44b
orehush <o.rehush@bvblogic.com>
2019-12-26 14:29:53 +0200

    Update lab7_sort.py

--- lab7_sort.py
+++ lab7_sort.py
@@ -16,19 +16,15 @@ arr = [[random.randint(0, 100) for _ in range(n)] for i in range(n)]
 print_matrix(arr)
 
 for i in range(n):
-    col = [arr[j][-2] for j in range(i, n)]
-    m = min(col)
-    index = col.index(m)
-    if arr[i][-2] < arr[index+i][-2]:
-        arr[i][-2], arr[index + i][-2] = arr[index+i][-2], arr[i][-2]
+  max_ = arr[i][-2]
+  index = i
+  for j in range(i+1, n):
+    if arr[j][-2] > max_:
+      max_ = arr[j][-2]
+      index = j
+  if arr[i][-2] < arr[index][-2]:
+    arr[i][-2], arr[index][-2] = arr[index][-2], arr[i][-2]
 
-print('-'*10)
+print('-' * 10)
 
 print_matrix(arr)
-# for i in range(n):
-#     for j in range(i, n):
-#         if arr[i] > arr[j]:
-#             arr[i], arr[j] = arr[j], arr[i]
-#
-# print(arr)
-# print(arr[0] + arr[-1])
